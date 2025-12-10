00a3dd0df79cefcd842dd0f0f8959ff49a4d3c32
Cyprien <cyprienarmand@gmail.com>
2025-12-10 15:02:12 -0500

    new db

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,6 @@
 // We switch to esm.sh which handles the module exports for Supabase much better in the browser
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://mvtaweywmemerwnkanco.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12dGF3ZXl3bWVtZXJ3bmthbmNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5OTI0MTYsImV4cCI6MjA4MDU2ODQxNn0.x0VZ7_s9yk915XLpdkz4r4L2wtcSEfFHg4jEMAioFHE"
-
+const SUPABASE_URL = "https://iqeifflmubyvlygremnr.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZWlmZmxtdWJ5dmx5Z3JlbW5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzOTQ0MzAsImV4cCI6MjA4MDk3MDQzMH0.eYNi0AnnJOCoV80KwenF7HIxy6OlwesbhLIcLhzkVnE";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
